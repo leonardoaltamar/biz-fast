07c0783b05309b80550b05d5ae2138109f291d46
Leonardo altamar <leonardoaltamar12@gmail.com>
2024-07-21 12:59:06 -0500

    instalando react router y empezando con la configuración de los temas de la aplicación

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
+import { AppRouter } from "./app.routes"
 
-
-import { ButtonAtom } from './components/atoms/button/button'
-import { InputAtom } from './components/atoms/input/input'
-import { LabelAtom } from './components/atoms/label/label'
-import { InputGroupMolecule } from './components/molecules/input-group/inputGroup'
 
 
 
@@ -11,33 +7,7 @@ function App() {
 
 
   return (
-    <div className='ml:container mx-auto p-8'>
-      <h1 className='mb-2 text-xl font-bold text-center mb-8'>Componentes</h1>
-
-      <div className='mb-8'>
-        <h2 className='mb-2 text-xl'>Inputs</h2>
-
-        <InputGroupMolecule className={'mb-5'}>
-          <InputAtom outlined={true} />
-          <LabelAtom>nombre</LabelAtom>
-        </InputGroupMolecule>
-
-        <InputGroupMolecule>
-          <InputAtom />
-          <LabelAtom>nombre</LabelAtom>
-        </InputGroupMolecule>
-
-      </div>
-
-      <div className='mb-8'>
-        <h2 className='mb-2 text-xl'>Buttons</h2>
-        <ButtonAtom className={'mb-3'}>Ingresar</ButtonAtom>
-        <ButtonAtom outlined={true}>Ingresar</ButtonAtom>
-      </div>
-
-
-
-    </div>
+    <AppRouter />          
   )
 }
 
---
+++ src/features/auth/pages/login/login.tsx
@@ -0,0 +1,7 @@
+export const LoginPage = () => {
+  return (
+    <div className="background-primary min-h-full h-screen w-full flex justify-center items-center">
+      <h1>login</h1>
+    </div>
+  )
+}
